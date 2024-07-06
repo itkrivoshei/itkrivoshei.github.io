084a5b995d757dd5f9471347683d481b462899b2
itkrivoshei <nikitakrivoshei@gmail.com>
2024-07-06 21:25:22 +0200

    refactor: Remove unused icon import in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,6 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
       },
     },
   ],

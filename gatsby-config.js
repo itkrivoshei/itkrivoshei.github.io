f049a22ab101023996aef54fde0c0da57527a717
itkrivoshei <nikitakrivoshei@gmail.com>
2024-07-06 21:25:27 +0200

    refactor: Remove unused manifest plugin in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,15 +19,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
   ],
 }

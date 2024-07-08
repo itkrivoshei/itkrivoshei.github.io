01daa436bdf3ca3783df8e5d0e9d41d91afb0e75
itkrivoshei <nikitakrivoshei@gmail.com>
2024-07-08 09:57:33 +0200

    refactor: Copy CNAME file to public directory during build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const fs = require("fs")
+const path = require("path")
+
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
@@ -9,4 +12,19 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+}
+
+/**
+ * @type {import('gatsby').GatsbyNode['onPostBuild']}
+ */
+exports.onPostBuild = () => {
+  const source = path.join(__dirname, "CNAME")
+  const destination = path.join(__dirname, "public", "CNAME")
+
+  if (fs.existsSync(source)) {
+    fs.copyFileSync(source, destination)
+    console.log("CNAME file copied to public directory")
+  } else {
+    console.error("CNAME file does not exist in the root directory")
+  }
 }

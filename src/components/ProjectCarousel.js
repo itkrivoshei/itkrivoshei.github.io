6dca955332e1e4b080301aed842ed2968de574b3
itkrivoshei <nikitakrivoshei@gmail.com>
2024-07-12 14:16:15 +0200

    refactor: Update ProjectCarousel component to use react-horizontal-scrolling-menu

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -1,57 +1,104 @@
-import React from "react"
-import Slider from "react-slick"
+import React, { useRef, useEffect } from "react"
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu"
+import "react-horizontal-scrolling-menu/dist/styles.css"
 import "./projectCarousel.css"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
 
-import exampleGif1 from "../images/OdinMonoWebApps.gif"
-import exampleGif2 from "../images/SelectionSortAndVisualizer.gif"
-import exampleGif3 from "../images/BasicsOfPredictiveAnalysis.gif"
+import monoWebAppsPng from "../images/OdinMonoWebApps.png"
+import selectionSortPng from "../images/SelectionSortAndVisualizer.png"
+import machineLearningPng from "../images/BasicsOfMachineLearningAndVisualisation.png"
+import predictiveAnalysisPng from "../images/BasicsOfPredictiveAnalysis.png"
 
 const projectData = [
   {
-    title: "Project One",
-    description: "Description of Project One",
-    link: "https://github.com/yourprofile/project-one",
-    image: exampleGif1,
+    title: "Odin Mono Web Apps",
+    description:
+      "A collection of web-based projects created with React, TypeScript, and Redux.",
+    link: "https://github.com/itkrivoshei/OdinMonoWebApps",
+    image: monoWebAppsPng,
   },
   {
-    title: "Project Two",
-    description: "Description of Project Two",
-    link: "https://github.com/yourprofile/project-two",
-    image: exampleGif2,
+    title: "Selection Sort Visualizer",
+    description:
+      "An interactive way to learn Selection Sort with visual tests.",
+    link: "https://github.com/itkrivoshei/SelectionSortAndVisualizer",
+    image: selectionSortPng,
   },
   {
-    title: "Project Three",
-    description: "Description of Project Three",
-    link: "https://github.com/yourprofile/project-three",
-    image: exampleGif3,
+    title: "Machine Learning And Visualisation",
+    description:
+      "Predict salaries based on years of experience using linear regression.",
+    link: "https://github.com/itkrivoshei/BasicsOfMachineLearningAndVisualisation",
+    image: machineLearningPng,
+  },
+  {
+    title: "Predictive Analysis",
+    description:
+      "Predict rental prices and classify luxury apartments in Berlin.",
+    link: "https://github.com/itkrivoshei/BasicsOfPredictiveAnalysis",
+    image: predictiveAnalysisPng,
   },
 ]
 
+const Arrow = ({ text, className, onClick }) => {
+  return (
+    <div className={`arrow ${className}`} onClick={onClick}>
+      {text}
+    </div>
+  )
+}
+
+const LeftArrow = () => {
+  const { scrollPrev } = React.useContext(VisibilityContext)
+
+  return <Arrow className="left-arrow" onClick={() => scrollPrev()} />
+}
+
+const RightArrow = () => {
+  const { scrollNext } = React.useContext(VisibilityContext)
+
+  return <Arrow className="right-arrow" onClick={() => scrollNext()} />
+}
+
 const ProjectCarousel = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    swipeToSlide: true,
-  }
+  const apiRef = useRef(null)
+
+  useEffect(() => {
+    const scrollContainer = apiRef.current?.scrollContainer
+
+    const handleScroll = () => {
+      if (!scrollContainer) return
+      const { scrollLeft, scrollWidth, clientWidth } = scrollContainer
+      const maxScrollLeft = scrollWidth - clientWidth
+
+      if (scrollLeft <= 0) {
+        scrollContainer.scrollLeft = maxScrollLeft - clientWidth
+      } else if (scrollLeft >= maxScrollLeft) {
+        scrollContainer.scrollLeft = clientWidth
+      }
+    }
+
+    scrollContainer?.addEventListener("scroll", handleScroll)
+    return () => scrollContainer?.removeEventListener("scroll", handleScroll)
+  }, [])
 
   return (
-    <Slider {...settings} className="project-carousel">
-      {projectData.map((project, index) => (
-        <div key={index} className="project-item">
-          <img src={project.image} alt={project.title} />
-          <h3>{project.title}</h3>
-          <p>{project.description}</p>
-          <a href={project.link} target="_blank" rel="noopener noreferrer">
-            View Project
+    <div className="project-carousel-wrapper">
+      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} ref={apiRef}>
+        {projectData.map((project, index) => (
+          <a
+            key={index}
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-item"
+          >
+            <img src={project.image} alt={project.title} />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
           </a>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </ScrollMenu>
+    </div>
   )
 }
 

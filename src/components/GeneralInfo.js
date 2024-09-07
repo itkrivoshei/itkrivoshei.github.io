7e9c95e669204bda08906c7d62727c07d6248f89
itkrivoshei <nikitakrivoshei@gmail.com>
2024-09-07 19:07:29 +0200

    refactor: Update GeneralInfo component layout and remove unnecessary code

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -5,14 +5,49 @@ import profilePhoto from "../images/profilePhoto.jpg"
 const GeneralInfo = () => {
   return (
     <section className="general-info">
-      <img
+      {/* Profile Photo */}
+      {/* <img
         src={profilePhoto}
         alt="Nikita Krivoshei"
         className="profile-photo"
-      />
-      <div>
-        <h1>Nikita Krivoshei</h1>
-        <h3>Front-End Engineer</h3>
+      /> */}
+
+      {/* Main Info */}
+      <div className="info-container">
+        <h1 className="name">Nikita Krivoshei</h1>
+        <h3 className="title">Front-End Engineer</h3>
+      </div>
+
+      {/* Links and Resume */}
+      <div className="links">
+        <p>
+          Find me on:
+          <a
+            href="https://www.linkedin.com/in/itkivoshei/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          and
+          <a
+            href="https://github.com/itkrivoshei"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+        <p>
+          <a
+            href="https://drive.google.com/file/d/1qTOYft9_fMY25XVUD5Ce2_P6yWD8a48_/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download My Resume
+          </a>
+        </p>
       </div>
     </section>
   )

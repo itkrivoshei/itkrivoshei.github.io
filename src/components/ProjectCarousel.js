cad8378c2106dea349364c533bdf8da7c38a5895
itkrivoshei <nikitakrivoshei@gmail.com>
2024-09-17 17:09:42 +0200

    Refactor ProjectCarousel.js to import projectData from separate file

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -2,46 +2,7 @@ import React, { useRef, useState } from "react"
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu"
 import "react-horizontal-scrolling-menu/dist/styles.css"
 import "./projectCarousel.css"
-
-import monoWebAppsPng from "../images/OdinMonoWebApps.png"
-import selectionSortPng from "../images/SelectionSortAndVisualizer.png"
-import machineLearningPng from "../images/BasicsOfMachineLearningAndVisualisation.png"
-import predictiveAnalysisPng from "../images/BasicsOfPredictiveAnalysis.png"
-
-const projectData = [
-  {
-    title: "Odin Mono Web Apps",
-    description:
-      "A collection of web-based projects created with React, TypeScript, and Redux.",
-    link: "https://github.com/itkrivoshei/OdinMonoWebApps",
-    image: monoWebAppsPng,
-    tags: ["React", "Redux", "TypeScript"],
-  },
-  {
-    title: "Selection Sort Visualizer",
-    description:
-      "An interactive way to learn Selection Sort with visual tests.",
-    link: "https://github.com/itkrivoshei/SelectionSortAndVisualizer",
-    image: selectionSortPng,
-    tags: ["JavaScript", "Sorting", "Algorithms"],
-  },
-  {
-    title: "Machine Learning And Visualisation",
-    description:
-      "Predict salaries based on years of experience using linear regression.",
-    link: "https://github.com/itkrivoshei/BasicsOfMachineLearningAndVisualisation",
-    image: machineLearningPng,
-    tags: ["Python", "Machine Learning", "Linear Regression"],
-  },
-  {
-    title: "Predictive Analysis",
-    description:
-      "Predict rental prices and classify luxury apartments in Berlin.",
-    link: "https://github.com/itkrivoshei/BasicsOfPredictiveAnalysis",
-    image: predictiveAnalysisPng,
-    tags: ["Python", "Pandas", "Data Analysis"],
-  },
-]
+import { projectData } from "./projectData"
 
 class DragDealer {
   clicked = false

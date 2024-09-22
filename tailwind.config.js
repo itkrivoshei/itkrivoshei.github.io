392c048b7893addb30c35ef315e99edf4a3f52a5
itkrivoshei <nikitakrivoshei@gmail.com>
2024-09-22 21:30:47 +0200

    Refactor tailwind.config.js to extend theme with custom colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dracula: {
+          background: '#282a36',
+          currentLine: '#44475a',
+          selection: '#44475a',
+          foreground: '#f8f8f2',
+          comment: '#6272a4',
+          cyan: '#8be9fd',
+          green: '#50fa7b',
+          orange: '#ffb86c',
+          pink: '#ff79c6',
+          purple: '#bd93f9',
+          red: '#ff5555',
+          yellow: '#f1fa8c',
+        },
+      },
+      fontFamily: {
+        sans: ['"Fira Code"', 'monospace'],
+      },
+    },
   },
   plugins: [],
-}
+};

4dd69ef29f0fa72e37aac2ed146e39214f0f1fae
itkrivoshei <nikitakrivoshei@gmail.com>
2024-09-23 15:38:18 +0200

    Refactor gatsby-config.ts to remove gatsby-plugin-google-gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,7 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     'gatsby-plugin-postcss',
-    'gatsby-plugin-google-gtag',
+    // 'gatsby-plugin-google-gtag',
     'gatsby-plugin-image',
     'gatsby-plugin-sitemap',
     {

0a422a6509903fc2e77d7724b4c54f194fe97aa8
itkrivoshei <nikitakrivoshei@gmail.com>
2024-09-28 20:52:48 +0200

    Refactor ProjectsBlock component to display a link to my GitHub profile

--- src/components/ProjectsBlock.tsx
+++ src/components/ProjectsBlock.tsx
@@ -1,41 +1,22 @@
 import React from 'react';
 
-const projects = [
-  {
-    name: 'Project Name',
-    description: 'Project description...',
-    link: 'https://project-link.com',
-  },
-];
-
 const ProjectsBlock: React.FC = () => {
   return (
     <section className='py-12 px-4 md:px-8 bg-dracula-currentLine'>
       <h2 className='text-3xl font-semibold text-dracula-foreground mb-6'>
         Projects
       </h2>
-      {projects.length > 0 ? (
-        projects.map((project, index) => (
-          <div key={index} className='mb-6'>
-            <h3 className='text-2xl font-bold text-dracula-green'>
-              {project.name}
-            </h3>
-            <p className='text-dracula-foreground mb-2'>
-              {project.description}
-            </p>
-            <a
-              href={project.link}
-              className='text-dracula-cyan hover:underline'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              View Project
-            </a>
-          </div>
-        ))
-      ) : (
-        <p className='text-dracula-foreground'>Projects will be added soon.</p>
-      )}
+      <p className='text-dracula-foreground'>
+        Please visit my GitHub profile to explore my projects:
+      </p>
+      <a
+        href='https://github.com/itkrivoshei'
+        className='text-dracula-pink hover:underline'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        github.com/itkrivoshei
+      </a>
     </section>
   );
 };
